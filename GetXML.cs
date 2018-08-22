df31ac1501732a1a9c906c95f94b60bf4abd2d09
bphnr900 <40142616+bphnr900@users.noreply.github.com>
2018-08-22 22:27:57 +0900

    Initial commit

---
+++ GetXML.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace FileCopy
+{
+    public class GetXML
+    {
+        public void Sample()
+        {
+            IEnumerable<XElement> files = XElement.Load(@"C:\Users\bphnr\source\repos\FileCopy\FileCopy\FileList.xml").Elements("Folders");
+            
+            foreach(XElement file in files)
+            {
+                Console.WriteLine(file.Elements("FolderName"));
+            }
+            
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace FileCopy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //xml読み込み
+            //IEnumerable<XElement> Folders = XElement.Load(@"C:\Users\bphnr\source\repos\FileCopy\FileCopy\FileList.xml").Elements("Folder");
+            IEnumerable<XElement> Folders = XElement.Load("FileList.xml").Elements("Folder");
+
+            //読み込み結果を確認
+            foreach (XElement folder in Folders)
+            {
+                //コピー元、コピー先のディレクトリを取得
+                string BeforePath = folder.Element("SourceDirectory").Value;
+                string AfterPath = folder.Element("DestinationDirectory").Value;
+
+                //取得したパスをもとにコピー
+                DirectoryCopy(BeforePath, AfterPath);
+            }
+        }
+
+        //ディレクトリのコピー
+        public static void DirectoryCopy(string SourcePath,string DestinationPath)
+        {
+            DirectoryInfo sourceDirectory = new DirectoryInfo(SourcePath);
+            DirectoryInfo destinationDirectory = new DirectoryInfo(DestinationPath);
+            //コピー先のディレクトリがなければ作成する
+            if(destinationDirectory.Exists == false)
+            {
+                destinationDirectory.Create();
+                destinationDirectory.Attributes = sourceDirectory.Attributes;
+            }
+            //ファイルのコピー
+            foreach(FileInfo fileinfo in sourceDirectory.GetFiles())
+            {
+                //同じファイルが存在していたら常に上書きする
+                fileinfo.CopyTo(destinationDirectory.FullName + @"\" + fileinfo.Name, true);
+            }
+            //ディレクトリのコピー(再起を利用)
+            foreach(System.IO.DirectoryInfo directoryInfo in sourceDirectory.GetDirectories())
+            {
+                DirectoryCopy(directoryInfo.FullName, destinationDirectory.FullName + @"\" + directoryInfo.Name);
+            }
+        }
+    }
+}
